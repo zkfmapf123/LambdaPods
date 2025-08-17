f8cd7033610ba7a3531ebfaf94b1ea18ec0916bb
leedonggyu <zkfmapf123@naver.com>
2025-08-18 00:50:40 +0900

    wip-8928807e

--- cmd/examples/dto/setting.go
+++ cmd/examples/dto/setting.go
@@ -1,11 +1,9 @@
 package dto
 
-// SettingAssumeRoleRequest represents the request to update AWS Assume Role ARN
 type SettingAssumeRoleRequest struct {
 	AssumeRoleARN string `json:"assume_role_arn" validate:"required" example:"arn:aws:iam::123456789012:role/MyRole"`
 }
 
-// SettingOpenAPIKeyRequest represents the request to update OpenAPI Key
 type SettingOpenAPIKeyRequest struct {
 	OpenAPIKey string `json:"openapi_key" validate:"required" example:"sk-1234567890abcdef"`
 }
